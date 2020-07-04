1925638cc1a9cab9f01b892beab78d01d4c8fd05
Alechu <alejo9726@gmail.com>
2020-07-04 16:31:53 -0300

    Agrego lo necesario para correr React

---
+++ gorona-react/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Gorona-Stats. A simple, GOlang powered, non-tracking and
+          ad-free wesbite for Coronavirus statistics.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
